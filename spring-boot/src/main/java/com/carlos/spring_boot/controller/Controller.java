59db8aebde6498f4b0a94d8b77c3351686335a79
Carlos Lopes <carlos.per.lopes@gmail.com>
2025-04-06 01:36:16 -0300

    ajuste

---
+++ spring-boot/src/main/java/com/carlos/spring_boot/controller/Controller.java
@@ -0,0 +1,28 @@
+package com.carlos.spring_boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carlos.spring_boot.model.Pessoa;
+
+
+
+@RestController
+public class Controller {
+
+    @GetMapping("/{nome}")
+    public String boasvindas(@PathVariable String nome) {
+        return "Seja bem vindo (a) " + nome;
+    }
+
+    @PostMapping("/pessoa")
+    public Pessoa pessoa(@RequestBody Pessoa p) {
+        return p;
+    }
+    
+    
+}
+
